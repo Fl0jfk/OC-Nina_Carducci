61e2d6167622eed60e9d54832a65812d3cb0af29
Flojfk <florian.hacqueville@hotmail.fr>
2023-07-12 15:56:21 +0200

    first commit

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,2 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
-});
+$(document).ready(function() {$('.gallery').mauGallery({columns: {xs: 1,sm: 2,md: 3,lg: 3,xl: 3},lightBox: !0,lightboxId: 'myAwesomeLightbox',showTags: !0,tagsPosition: 'top'});});
+window.onload=function(){setTimeout(() => {document.querySelector(".loader").style.display="none"}, 300)};    
